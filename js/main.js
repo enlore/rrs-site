f8d2c646e14de6989fdbcc5ae30efcf6d035bb94
enlore <n.e.lorenson@gmail.com>
2014-04-07 19:36:43 -0500

    Contact us, meet us, nav colors

--- js/main.js
+++ js/main.js
@@ -2,4 +2,9 @@ $(document).ready(function () {
     $('#sponsor-us').on('click', function (e) {
         $('#sponsor-form').toggleClass('active')
     })
+
+    $('#fresh-meat-button').on('click', function (e) {
+        console.log('butts')
+        $('#fresh-meat-form').toggleClass('active') 
+    })
 })
